ac00a35d78b5856a1a6d459c9e7f8db57ba4164d
Aishwarya <ishans632@gmail.com>
2019-03-31 23:41:11 +0530

    computer logic

--- ScarnesDice/app/src/main/java/com/example/scarnesdice/MainActivity.java
+++ ScarnesDice/app/src/main/java/com/example/scarnesdice/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.scarnesdice;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,53 +20,80 @@ public class MainActivity extends AppCompatActivity {
     private static int user_turn_score = 0;
     private static int comp_turn_score = 0;
 
-    private static boolean isPlayerChance = true;
-
     private Random random = new Random();
+
+    // Three Buttons
+    private Button rollButton;
+    private Button holdButton;
+    private Button resetButton;
+
+    // Text Views
+    private TextView turnLabel;
+    private TextView turnScoreText;
+    private TextView playerOverAllScoreText;
+    private TextView compOverallScoreText;
+
+    // Image View
+    private ImageView diceImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button rollButton = (Button) findViewById(R.id.roll_btn);
-        Button holdButton = (Button) findViewById(R.id.hold_btn);
-        Button resetButton = (Button) findViewById(R.id.reset_btn);
+        rollButton = (Button) findViewById(R.id.roll_btn);
+        holdButton = (Button) findViewById(R.id.hold_btn);
+        resetButton = (Button) findViewById(R.id.reset_btn);
+
+        turnScoreText = (TextView) findViewById(R.id.turn_score);
+        turnLabel = (TextView) findViewById(R.id.turn_label_text);
+        playerOverAllScoreText = (TextView) findViewById(R.id.player_score);
+        compOverallScoreText = (TextView) findViewById(R.id.comp_score);
 
 
+        diceImage = (ImageView) findViewById(R.id.dice_image);
+
+        /*
+            ROLL DICE CLICK LISTENER
+         */
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                TextView turnScore = (TextView) findViewById(R.id.turn_score);
-
                 int dice_val = random.nextInt(6) + 1;
+                changeDiceImage(dice_val);
                 if(dice_val == 1) {
-                    isPlayerChance = !isPlayerChance;
                     user_turn_score = 0;
-
-                    Log.d("test", "NOW COMPUTER CHANCE");
+                    computerTurn();
                 }else{
                     user_turn_score+= dice_val;
                 }
-                turnScore.setText(("" +  user_turn_score));
-                changeDiceImage(dice_val);
+                turnScoreText.setText(("" +  user_turn_score));
             }
         });
 
+
+        /*
+            HOLD CLICK LISTENER
+         */
         holdButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView userOverallScore = (TextView) findViewById(R.id.player_score);
+                // COMPUTE
                 user_overall_score += user_turn_score;
                 user_turn_score = 0;
 
-                userOverallScore.setText(("" + user_overall_score));
+                // DISPLAY
+                playerOverAllScoreText.setText(("" + user_overall_score));
 
+                // COMPUTER CHANCE
+                computerTurn();
             }
         });
 
-
+        /*
+            RESET CLICK LISTENER
+         */
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changeDiceImage(Integer dice_val){
-        ImageView diceImage = (ImageView) findViewById(R.id.dice_image);
         String dice_id = "dice"+ dice_val;
 
         int resId = 0;
@@ -103,7 +131,37 @@ public class MainActivity extends AppCompatActivity {
         diceImage.setImageDrawable(getResources().getDrawable(resId));
     }
 
-    private void computerTurn(){
-        // TODO: COMPUTER TURN
+    private void computerResetValues() {
+        compOverallScoreText.setText(("" + comp_overall_score));
+        changeDiceImage(1);
+        turnLabel.setText("PLAYER TURN SCORE");
+        turnScoreText.setText(("" + 0));
+        rollButton.setEnabled(true);
+        holdButton.setEnabled(true);
+        comp_turn_score = 0;
+    }
+
+    private void computerTurn() {
+        turnScoreText.setText(("" + 0));
+        turnLabel.setText("COMP TURN SCORE");
+        rollButton.setEnabled(false);
+        holdButton.setEnabled(false);
+
+        int comp_dice_val = random.nextInt(6) + 1;
+
+        while(comp_turn_score < 10 && comp_dice_val != 1){
+            comp_turn_score += comp_dice_val;
+            Log.d("test" , ("" + comp_turn_score + ", DICE: " + comp_dice_val));
+            comp_dice_val = random.nextInt(6) + 1;
+        }
+
+        Log.d("text", ("OUT: " + comp_turn_score));
+
+        if(comp_turn_score >= 10){
+            // HOLD
+            comp_overall_score += comp_turn_score;
+        }
+
+        computerResetValues();
     }
 }
