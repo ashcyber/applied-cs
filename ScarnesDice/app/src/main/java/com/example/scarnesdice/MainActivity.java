bc7cb4acaaa6c399dd46a93832be76ea7724c01f
Aishwarya <ishans632@gmail.com>
2019-04-01 01:43:31 +0530

    Update MainActivity.java

--- ScarnesDice/app/src/main/java/com/example/scarnesdice/MainActivity.java
+++ ScarnesDice/app/src/main/java/com/example/scarnesdice/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.scarnesdice;
 
-import android.graphics.Color;
 import android.os.Handler;
-import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
-import java.nio.channels.Channel;
 import java.util.Random;
 import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private static int user_overall_score = 0;
